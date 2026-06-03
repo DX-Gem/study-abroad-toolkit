a0fc814f57c3c3e6b6098cf4dd9b7299b07ee654
Liu Dianxun <2183862292@qq.com>
2026-06-03 18:02:45 +0800

    release: v3.5 版本号升至40

--- frontend/update-checker.js
+++ frontend/update-checker.js
@@ -1,6 +1,6 @@
 // 应用内更新检查器
 (function() {
-    var CURRENT_VERSION = 39;
+    var CURRENT_VERSION = 40;
     var VERSION_URL = 'https://gitee.com/dianxun-liu/study-abroad-toolkit/raw/main/version.json';
 
     setTimeout(checkUpdate, 2500);
